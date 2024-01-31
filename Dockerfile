fb63c0ee5a2fda7fa56e1e2eedda582467752294
tarpha <tarpha@gmail.com>
2024-01-31 22:13:01 +0900

    add health

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,6 @@ WORKDIR /app
 COPY ./requirements.txt /app/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
 COPY ./app /app
+EXPOSE 80
+VOLUME "/logs"
 CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
--- app/main.py
+++ app/main.py
@@ -4,14 +4,24 @@ from LogItem import LogItem
 from fastapi import FastAPI
 
 logger = logging.getLogger('appender_logger')
-fileHandler = CompressingRotatingFileHandler("../logs/log_appender.log", maxBytes=10000000, backupCount=9)
-formatter = logging.Formatter("[%(asctime)s][%(levelname)s]%(message)s", "%Y-%m-%d %H:%M:%S")
-fileHandler.setFormatter(formatter)
-logger.addHandler(fileHandler)
 
 app = FastAPI()
 
+@app.get("/health")
+def health_check():
+    return "healthy"
+
 @app.post("/logging")
 def log_appender(logItem: LogItem):
+    fileHandler = None
+    if(logItem.add_time_field):
+        formatter = logging.Formatter("[%(asctime)s][%(levelname)s]%(message)s", "%Y-%m-%d %H:%M:%S")
+    else:
+        formatter = logging.Formatter("[%(levelname)s]%(message)s")
+    
+    fileHandler = CompressingRotatingFileHandler("../logs/log_appender.log", maxBytes=10000000, backupCount=9)
+    fileHandler.setFormatter(formatter)
+    logger.addHandler(fileHandler)
+
     logger.log(logItem.level.value, logItem.message)
     return {"log_devel": logItem.level, "log_message": logItem.message}
