f8949c53ab1409272ab82fa105c8201834d3b259
tarpha <tarpha@gmail.com>
2024-01-31 22:42:47 +0900

    dynamic handler

--- app/main.py
+++ app/main.py
@@ -3,7 +3,15 @@ from CompressingRotatingFileHandler import CompressingRotatingFileHandler
 from LogItem import LogItem
 from fastapi import FastAPI
 
+addTimeField = False
 logger = logging.getLogger('log_appender')
+formatter = logging.Formatter("[%(levelname)s]%(message)s")
+fileHandler = CompressingRotatingFileHandler("../logs/log_appender.log", maxBytes=10000000, backupCount=9)
+fileHandler.setFormatter(formatter)
+logger.addHandler(fileHandler)
+consoleHandler = logging.StreamHandler()
+consoleHandler.setFormatter(formatter)
+logger.addHandler(consoleHandler)
 
 app = FastAPI()
 
@@ -13,15 +21,17 @@ def health_check():
 
 @app.post("/logging")
 def log_appender(logItem: LogItem):
-    fileHandler = None
-    if(logItem.add_time_field):
-        formatter = logging.Formatter("[%(asctime)s][%(levelname)s]%(message)s", "%Y-%m-%d %H:%M:%S")
-    else:
-        formatter = logging.Formatter("[%(levelname)s]%(message)s")
-    
-    fileHandler = CompressingRotatingFileHandler("../logs/log_appender.log", maxBytes=10000000, backupCount=9)
-    fileHandler.setFormatter(formatter)
-    logger.addHandler(fileHandler)
+    global addTimeField
+    if(addTimeField != logItem.add_time_field):
+        if(logItem.add_time_field):
+            formatter = logging.Formatter("[%(asctime)s][%(levelname)s]%(message)s", "%Y-%m-%d %H:%M:%S")
+        else:
+            formatter = logging.Formatter("[%(levelname)s]%(message)s")
+        fileHandler.setFormatter(formatter)
+        logger.addHandler(fileHandler)
+        consoleHandler.setFormatter(formatter)
+        logger.addHandler(consoleHandler)
+        addTimeField = logItem.add_time_field
 
     logger.log(logItem.level.value, logItem.message)
     return {"log_devel": logItem.level, "log_message": logItem.message}
